05105ab77214e3967160791381a39b8059e1d2e1
mariohernandezv <mariohernandezv@yahoo.com.ar>
2016-08-04 12:53:53 -0500

    Ruby tr

--- pascalTriangle.rb
+++ pascalTriangle.rb
@@ -1,18 +1,20 @@
-def pascalsTriangle(arreglo_previo=[], c=1, n=5)
+def pascalsTriangle(n=8, c=1, arreglo_previo=[], salida=[])
 	arreglo_final= []
-	arreglo_auxiliar = []
 	if c == 1 
-		arr1 = [1]
+		salida += (arreglo_final = [1])
+		n > 1 ? pascalsTriangle(n, (c + 1), arreglo_final, salida) : salida
 	elsif c == 2
-		arr1 = [1,1]
-	elsif c == n
-		arreglo_previo
+	 	salida += (arreglo_final = [1,1])
+	 	pascalsTriangle(n, (c + 1), arreglo_final, salida)
+	elsif c == (n + 1)
+		salida
 	else
 		arreglo_final[0] = 1
-		arreglo_final[c] = 1
-		[*1...c].each do |num|
-			arreglo_final[num] = arreglo_previo[num] + arreglo_previo[num+1]
-			pascalsTriangle(arreglo_final, c+1, n)
+		arreglo_final[c - 1] = 1
+		[*1...(c - 1)].each do |num|
+			arreglo_final[num] = ( arreglo_previo[ (num - 1) ] + arreglo_previo[ (num) ] )
 		end
+		salida += arreglo_final
+		pascalsTriangle(n, (c + 1), arreglo_final, salida)
 	end
 end
---
+++ rot13.rb
@@ -0,0 +1,4 @@
+def rot13(message)
+	
+	message.tr('a-zA-Z', 'n-za-mN-ZA-M')
+end
