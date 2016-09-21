f8c8f61378a9a3f4a3bb160748f7f786d761a8eb
mariohernandezv <mariohernandezv@yahoo.com.ar>
2016-09-21 08:50:35 -0500

    Choose best sum

--- choose_best_sum.rb
+++ choose_best_sum.rb
@@ -1,10 +1,15 @@
-def choose_best_sum(t, k, ls)
-    if !ls
-    	nill
+def choose_best_sum(t = 20, k = 3, ls = [1,2,3,4,5,6,7,8,9])
+	menor = 0
+    if ls.empty?
+    	nil
     else
-    	ls.sort.each do |m|
-    		if m>
+    	ls.each.with_index do |m,i|
+    		if ((m + menor) < t ) && ( k > 0 )
+    			menor += m
+    			k -= 1
+    		end
     	end
+    	menor
     end
 
 end
